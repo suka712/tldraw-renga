67ab01ba277f03fd0979697a9924b0a12105c6f4
khiem <khiemnguyen.hye@gmail.com>
2026-02-18 18:27:31 +0700

    Init shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import { cloudflare } from '@cloudflare/vite-plugin'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path"
+
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
 	return {
 		plugins: [cloudflare(), react(), tailwindcss()],
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "./src"),
+			},
+		},
 	}
 })
